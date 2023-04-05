c57f72a15c13a942ec36bf866038711e59ecf8b6
litao <2532566074@qq.com>
2023-04-05 10:38:24 +0800

    uni-appm项目初始化

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const resolve = (p) => {
+  return path.resolve(__dirname, p);
+};
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./**/*.vue"].map(resolve),
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
